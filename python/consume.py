b770d0db985bd2314f0c43c577c83448637e7af7
kgorman <kgorman@me.com>
2016-08-11 13:32:44 -0700

    consumer, coupla minor fixes

--- python/consume.py
+++ python/consume.py
@@ -6,8 +6,7 @@ from kafka import KafkaConsumer
 EVENTADOR_KAFKA_TOPIC = "brewery"  # any topic name, will autocreate if needed
 EVENTADOR_BOOTSTRAP_SERVERS = "my bootstrap servers"  # value from deployments tab in UI
 
-consumer = KafkaConsumer(bootstrap_servers=EVENTADOR_BOOTSTRAP_SERVERS)
-consumer.assign([TopicPartition(EVENTADOR_KAFKA_TOPIC, 2)])
+consumer = KafkaConsumer(EVENTADOR_KAFKA_TOPIC, bootstrap_servers=EVENTADOR_BOOTSTRAP_SERVERS)
 
 for msg in consumer:
     print msg
