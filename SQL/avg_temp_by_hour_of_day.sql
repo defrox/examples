338046d3b661cd24b6a51fe268d3e3e92b8cd5b3
kgorman <kgorman@me.com>
2016-11-02 18:22:55 -0500

    fixes #19

--- SQL/avg_temp_by_hour_of_day.sql
+++ SQL/avg_temp_by_hour_of_day.sql
@@ -2,5 +2,5 @@
 CREATE CONTINUOUS VIEW brewery_temps_by_hourofday AS
 SELECT date_part('hour', arrival_timestamp) as ts,
 avg((payload->>'temp'::text)::numeric)
-FROM brewery_stream
+FROM defaultsink_stream
 GROUP BY ts;
--- SQL/avg_temp_by_sensor.sql
+++ SQL/avg_temp_by_sensor.sql
@@ -1,5 +1,6 @@
 -- average temperature of sensors over the last 5 minutes by sensor name
 CREATE CONTINUOUS VIEW brewery_sensor_temps WITH (max_age = '5 minutes') AS
-SELECT payload->>'sensor', AVG((payload->>'temp'::text)::numeric)
-FROM brewery_stream
+SELECT payload->>'sensor' as sensor,
+AVG((payload->>'temp'::text)::numeric)
+FROM defaultsink_stream
 GROUP BY payload->>'sensor';
